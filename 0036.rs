37ea43100593f86e03ee8ab37b7c619bf49098db
Shiwei Zhang <ylxdzsw@gmail.com>
2020-03-28 23:34:48 +0800

    valid sudoku

---
+++ 0036.rs
@@ -0,0 +1,126 @@
+// Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+// Each row must contain the digits 1-9 without repetition.
+// Each column must contain the digits 1-9 without repetition.
+// Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+// A partially filled sudoku which is valid.
+
+// The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+// Example 1:
+
+// Input:
+// [
+//   ["5","3",".",".","7",".",".",".","."],
+//   ["6",".",".","1","9","5",".",".","."],
+//   [".","9","8",".",".",".",".","6","."],
+//   ["8",".",".",".","6",".",".",".","3"],
+//   ["4",".",".","8",".","3",".",".","1"],
+//   ["7",".",".",".","2",".",".",".","6"],
+//   [".","6",".",".",".",".","2","8","."],
+//   [".",".",".","4","1","9",".",".","5"],
+//   [".",".",".",".","8",".",".","7","9"]
+// ]
+// Output: true
+// Example 2:
+
+// Input:
+// [
+//   ["8","3",".",".","7",".",".",".","."],
+//   ["6",".",".","1","9","5",".",".","."],
+//   [".","9","8",".",".",".",".","6","."],
+//   ["8",".",".",".","6",".",".",".","3"],
+//   ["4",".",".","8",".","3",".",".","1"],
+//   ["7",".",".",".","2",".",".",".","6"],
+//   [".","6",".",".",".",".","2","8","."],
+//   [".",".",".","4","1","9",".",".","5"],
+//   [".",".",".",".","8",".",".","7","9"]
+// ]
+// Output: false
+// Explanation: Same as Example 1, except with the 5 in the top left corner being 
+//     modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+// Note:
+
+// A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+// Only the filled cells need to be validated according to the mentioned rules.
+// The given board contain only digits 1-9 and the character '.'.
+// The given board size is always 9x9.
+
+#[allow(clippy::needless_range_loop)]
+fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
+    // row
+    for i in 0..9 {
+        let mut x = vec![false; 9];
+        for j in 0..9 {
+            if let Some(c) = board[i][j].to_digit(10) {
+                let c = (c - 1) as usize;
+                if x[c] {
+                    return false
+                }
+                x[c] = true
+            }
+        }
+    }
+
+    // column
+    for i in 0..9 {
+        let mut x = vec![false; 9];
+        for j in 0..9 {
+            if let Some(c) = board[j][i].to_digit(10) {
+                let c = (c - 1) as usize;
+                if x[c] {
+                    return false
+                }
+                x[c] = true
+            }
+        }
+    }
+
+    // block
+    for a in 0..3 {
+        for b in 0..3 {
+            let mut x = vec![false; 9];
+            for i in (3*a)..(3*a+3) {
+                for j in (3*b)..(3*b+3) {
+                    if let Some(c) = board[i][j].to_digit(10) {
+                        let c = (c - 1) as usize;
+                        if x[c] {
+                            return false
+                        }
+                        x[c] = true
+                    }
+                }
+            }
+        }
+    }
+
+    true
+}
+
+fn main() {
+    assert!(is_valid_sudoku(vec![
+        vec!['5', '3', '.', '.', '7', '.', '.', '.', '.'],
+        vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+        vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+        vec!['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+        vec!['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+        vec!['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+        vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+        vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+        vec!['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+    ]));
+    assert!(!is_valid_sudoku(vec![
+        vec!['8', '3', '.', '.', '7', '.', '.', '.', '.'],
+        vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+        vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+        vec!['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+        vec!['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+        vec!['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+        vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+        vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+        vec!['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+    ]));
+}
+
+// faster than 100.00%, less memory than 100.00%.
